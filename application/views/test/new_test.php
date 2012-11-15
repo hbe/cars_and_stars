435e1672a911d353bd5b9e6e3f83dcadef5cedd3
Johan Syah <jsyah@hbe.com.au>
2012-11-16 09:15:21 +1100

    installing some jquery
    I put some jquery and hide/show script into the "start a new test" page

--- application/views/test/new_test.php
+++ application/views/test/new_test.php
@@ -1,3 +1,4 @@
+<script src="/styles/js/jquery.js" type="text/JavaScript"></script>
 <div class="content">
 		<div class="title">
 			<h3>Start a New Test</h3>
@@ -6,27 +7,75 @@
 			<div class="test-content">
 				<p>Please select a Book to begin.</p>
 			<form  method="post" action="/test">
-			<?php 
-			foreach($books as $list)
-			{
-				?>
-				<label class="radio inline">
-				<input type="radio" name="book" style="border:1px solid #000; width:50px;" id="optionsRadios1" value="<?php echo $list->book ?>" checked>
-				<?php echo $list->book ?>
-				</label>
-				<?php
-			}
 
-			?>
+			<br />
+			Enter the student ID: <input type="textbox" name="student-id" />
+			<br />
+			<br />
+			<div id="pick-group">
+				Pick your test group: 
+				<select>
+					<option>Choose here</option>
+				</select>
+			
+				<button type="button" class="btn" onclick="showCreateNewTestGroup()">Create a new test group</button>
+			</div> 
+			<div id="new-group">
+				<button type="button" class="btn" onclick="showPickTestGroup()">Choose your existing test group</button> 
+			</div>
+			<br />
+			<div id="radio">
+				<?php 
+				foreach($books as $list)
+				{
+					?>
+					<label class="radio inline">
+					<input type="radio" name="book" id="optionsRadios1" value="<?php echo $list->book ?>" checked>
+					<?php echo $list->book ?>
+					</label>
+					<?php
+				}
+
+
+				?>
+			</div>
+			
 			<br />
 			<?php
 			echo (isset($empty))? $empty . "<br />" : "";
 			?>
+			
 			<br />
+			<input type="hidden" id="createOrPick" ></input>
 
    			<button class="btn btn-primary pull-left" type="submit">Begin Test</button>
 
 			</form>
 
 			</div>
+
 </div>
+<script type="text/JavaScript">
+	$('#radio').hide();
+	$('#new-group').hide();
+	$("#createOrPick").val("0");
+</script>
+<script type"text/JavaScript">
+
+	function showCreateNewTestGroup()
+	{
+		$('#radio').show('slow');
+		$('#pick-group').hide();
+		$('#new-group').show('slow');
+		$("#createOrPick").val("1");
+	}
+
+	function showPickTestGroup()
+	{
+		$('#radio').hide();	
+		$('#pick-group').show('slow');
+		$('#new-group').hide();
+		$("#createOrPick").val("0");
+	}
+
+</script>
